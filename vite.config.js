8c6066d98c5de78c6b75195e032895c073dfd6ec
Angela Aguila <angela.aguila@gmail.com>
2024-08-11 00:17:59 -0400

    Update json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/04-01Pizzeria",
+  base: "/04-03Pizzeria",
   plugins: [react()],
 })
